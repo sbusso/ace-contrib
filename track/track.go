0af02633ffefcebc91f55db92c8aa0af01b69689
Stéphane Busso <stephane.busso@gmail.com>
2015-09-27 19:22:30 +0700

    update tracker

--- track/track.go
+++ track/track.go
@@ -3,21 +3,47 @@ package track
 import (
   "github.com/plimble/ace"
   "code.google.com/p/go-uuid/uuid"
+  "github.com/influxdb/influxdb/client"
+  // "time"
 )
 
 const (
-    Tracker     = "__fat"
-  )
-// Middleware sets Tracking cookie
-func Track() ace.HandlerFunc {
+  Tracker  = "_t_"
+  V_ID    = "_v_id"
+  Measurement = "tracking"
+)
 
+// Middleware sets Tracking cookie
+func Track(i *client.Client) ace.HandlerFunc {
   return func(c *ace.C) {
     session := c.Sessions(Tracker)
-    vid := session.GetString("__vid","")
-    if vid == "" {
-      vid = uuid.New()
-      session.Set("__vid", vid)
+    _vid := session.GetString(V_ID,"")
+    if _vid == "" {
+      _vid = uuid.New()
+      session.Set(V_ID, _vid)
     }
 
+    var pts = make([]client.Point, 1)
+
+    pts[0] = client.Point{
+      Measurement: Measurement,
+      Tags: map[string]string{
+        "event": "view",
+        "vid": _vid,
+      },
+      Fields: map[string]interface{}{
+        "value": 1,
+      },
+      // Time: time.Now(),
+      Precision: "s",
+    }
+
+    bps := client.BatchPoints{
+      Points:          pts,
+      Database:      "mixevents",
+      RetentionPolicy: "default",
+  	}
+    i.Write(bps)
+    c.Next()
   }
 }
